12c0f81d56b02cba19f38b056ab21dfe383777f4
pixelbubble <75697623+pixelbubble@users.noreply.github.com>
2022-01-30 21:08:39 +0100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+#To complete
+keyword="google|amazon" #Keywords to complete
+mail="hello@world.com" #Email to complete
+
 sudo python3 domaindownload.py
-sudo chown -R ubuntu:ubuntu "$(\ls -1dt ./*/ | head -n 1)" #Your username:username
 cd  "$(\ls -1dt ./*/ | head -n 1)" 
-egrep "google|amazon" domain-names.txt > result.txt #Keywords to complete
-mail -s "Domain Alerting" my@email.com < result.txt #Email to complete
+egrep $keyword domain-names.txt > result.txt
+mail -s "Domain Alerting $keyword" $mail < result.txt
