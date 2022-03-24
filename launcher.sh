d40e232bec842cb30179e2fe2c6c502ebcdc040b
pudsec <54846158+pudsec@users.noreply.github.com>
2022-03-24 20:14:37 +0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 #To complete
-keyword="google|amazon" #Keywords to complete
+export keyword="google|amazon" #Keywords to complete
 mail="hello@world.com" #Email to complete
 
-sudo python3 domaindownload.py
-cd  "$(\ls -1dt ./*/ | head -n 1)" 
-egrep $keyword domain-names.txt > result.txt
-mail -s "Domain Alerting $keyword" $mail < result.txt
+python3 domaindownload.py | mail -E -s "Domain Alerting $keyword" $mail
